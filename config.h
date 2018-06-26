ccb3f36e6688feb35ba327bef61b24490d767fae
Oliver Rausch <oliverrausch99@gmail.com>
2018-06-26 23:56:20 +0200

    Add keybinding and attachaside patch

--- config.h
+++ config.h
@@ -72,11 +72,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "j4-dmenu-desktop", NULL };
 static const char *termcmd[]  = { "x-terminal-emulator", NULL };
+static const char *lockcmd[]  = { "xscreensaver-command", "-lock", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ Mod1Mask,                     XK_space,  spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,		XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,				XK_Return, spawn, 		   {.v = termcmd } },
+	{ MODKEY|ShiftMask,				XK_l,	   spawn, 		   {.v = lockcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -86,7 +88,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,             		XK_q,      killclient,     {0} },
+	{ MODKEY,             			XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.i = 0} },
 	{ MODKEY,                       XK_f,      setlayout ,     {.i = 1} },
 	{ MODKEY,                       XK_p,      setlayout,      {.i = 2} },
